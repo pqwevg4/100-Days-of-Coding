9db144c23480b3abd522cced98f7504330d59a04
Vinay <164609179+pqwevg4@users.noreply.github.com>
2024-07-01 19:25:01 +0530

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,8 @@
+print("Hello and Welcome to Band Name Generator !!")
+print("Just enter some of your details >>")
+
+city_name = input("What city did you grow up in ?")
+
+pet_name = input("Okay Gr8 , now enter name of your pet >>")
+
+print(f"Your unique BAND name is {city_name}{pet_name} , \nEnjoy !!")
